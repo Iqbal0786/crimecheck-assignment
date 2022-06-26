50b2faeea2a712e2cc19570b8b89ba6097d99990
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-26 10:41:41 +0530

    added component

---
+++ src/components/NoticePage.jsx
@@ -0,0 +1,17 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+export default function NoticePage() {
+  return (
+   <>
+ <AppBar>
+
+    <Toolbar>
+        <Typography>Welcome Iqbal@123</Typography>
+    </Toolbar>
+ </AppBar>
+   
+   
+   </>
+  )
+}
