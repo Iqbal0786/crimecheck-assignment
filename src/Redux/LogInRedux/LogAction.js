9f01e8ad5a536075020369ff0f5198cdb3d09a5e
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-26 12:56:08 +0530

    added redux

---
+++ src/Redux/LogInRedux/LogAction.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+  // ACTIONS
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+
+export const LOGIN_LODING = 'LOGIN_LODING';
+
+export const LOGOUT = 'LOGOUT';
+
+// ACTION CREATORS
+
+export const loginLoding = () => ({ type: LOGIN_LODING });
+
+export const loginError = () => ({ type: LOGIN_ERROR });
+
+export const loginSuccess = (payload) => ({ type: LOGIN_SUCCESS, payload });
+
+export const logoutUser = () => ({ type: LOGOUT})
+
+export const loginSuccessData = (data,navigate ) => (dispatch) => {
+
+    dispatch(loginLoding());
+    axios.post("localhost:1212/users/login", data).then(({ data }) => {
+        dispatch(loginSuccess(data))
+        console.log(data)
+        alert("Logged in Successfully")
+        setTimeout(() => { navigate("/"); }, 3000)
+    }).catch((err) => {
+        dispatch(loginError())
+      alert("Please check your email or password")
+    });
+}
---
+++ src/Redux/RegisterRedux/Register/RegisterAction.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+export const REGISTER_LOADING="REGISTER_LOADING";
+export const REGISTER_ERROR="REGISTER_ERROR";
+export const REGISTER_SUCCESS="REGISTER_SUCCESS";
+
+export const register_loading=()=>({type:REGISTER_LOADING});
+export const register_error=()=>({type:REGISTER_ERROR});
+export const register_success=(payload)=>({type:REGISTER_SUCCESS , payload});
+
+export const RegisterHandler=(data,navigate)=>(dispatch)=>{
+
+          dispatch(register_loading())
+    axios
+    .post("localhost:1212/users/register", data)
+    .then((res) => {
+      if (res) {
+          dispatch(register_success(res.data))
+        notify();
+        setTimeout(()=>{navigate("/Login")},3000)
+      }
+    })
+    .catch((error) => {
+      notify2();
+      dispatch(register_error())
+    });         
+}
+
